e94fd6e2e78655910e6ee5bd54f7857e6aaaf956
Roman <jenrom@gmail.com>
2009-11-10 18:34:41 +0100

    Partial fix for the dialog launcher dialog resolving issuer

--- SRC/App/LogSpy.UI/Views/Dialogs/DialogLauncher.cs
+++ SRC/App/LogSpy.UI/Views/Dialogs/DialogLauncher.cs
@@ -1,4 +1,5 @@
 using Microsoft.Practices.ServiceLocation;
+using StructureMap;
 
 namespace LogSpy.UI.Views.Dialogs
 {
@@ -6,7 +7,9 @@ namespace LogSpy.UI.Views.Dialogs
     {
         public void LaunchFor<TDialogHandler>(TDialogHandler handler)
         {
-            var dialog = ServiceLocator.Current.GetInstanceWith<IDialog<TDialogHandler>>(handler);
+            var dialog = ObjectFactory.With(handler).GetInstance<IDialog<TDialogHandler>>();
+            //TODO: Need to find why the above code works but the code underneath doesn't works
+            //var dialog = ServiceLocator.Current.GetInstanceWith<IDialog<TDialogHandler>>(handler);
             dialog.ShowDialog();
         }
     }
